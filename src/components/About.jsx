e1ed558d14c8d9c07e7ac057f0ebb4515b11db1d
KatarzynaMenio <meniokatarzyna@gmail.com>
2024-05-06 20:38:17 +0200

    #1e home page - about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,18 @@
+import decoration_img from "../assets/Decoration.svg";
+import signature from "../assets/Signature.svg";
+
 export default function About() {
     return (
-        <div id="about-section">
-            About
+        <div id="about-section" className="about-section-style">
+            <div className="about-left-side">
+                <div className="about-content">
+                    <h3>O nas</h3>
+                    <img src={decoration_img} alt="decoration" className="info-decor"/>
+                    <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                    <span><img src={signature} alt="signature"/></span>
+                </div>
+            </div>
+            <div className="about-section-img"></div>
         </div>
     )
 }
